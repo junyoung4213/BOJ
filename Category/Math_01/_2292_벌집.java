a341e4c228c95a6e0a07208826daf587c339e749
junyoung4213 <junyoung4213@gmail.com>
2020-03-10 13:43:16 +0900

    2020/03/10

---
+++ Category/Math_01/_2292_벌집.java
@@ -0,0 +1,53 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/*문제
+
+https://www.acmicpc.net/problem/2292
+
+위의 그림과 같이 육각형으로 이루어진 벌집이 있다(그림은 위 링크 참조)
+
+그림에서 보는 바와 같이 중앙의 방 1부터 시작해서 이웃하는 방에 돌아가면서 
+
+1씩 증가하는 번호를 주소로 매길 수 있다. 
+
+숫자 N이 주어졌을 때, 벌집의 중앙 1에서 N번 방까지 최소 개수의 방을 지나서 갈 때 
+
+몇 개의 방을 지나가는지(시작과 끝을 포함하여)를 계산하는 프로그램을 작성하시오. 
+
+예를 들면, 13까지는 3개, 58까지는 5개를 지난다.
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 1,000,000,000)이 주어진다.
+
+출력
+입력으로 주어진 방까지 최소 개수의 방을 지나서 갈 때 몇 개의 방을 지나는지 출력한다.*/
+
+public class _2292_02 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		int i = 1;
+		int cnt = 0;
+		N -= 1;
+		cnt++;
+		while (N > 0) {
+			N -= 6 * i;
+			cnt++;
+			i++;
+		}
+
+		bw.write(cnt + "");
+		bw.flush();
+		bw.close();
+		br.close();
+
+	}
+}
