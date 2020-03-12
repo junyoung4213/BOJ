54c704ecbbf147a5eec2cd21bdb7efc91fb1e4e1
junyoung4213 <junyoung4213@gmail.com>
2020-03-12 14:07:12 +0900

    2020/03/12

---
+++ Category/Math_01/_2869_달팽이는_올라가고_싶다.java
@@ -0,0 +1,43 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/*문제
+땅 위에 달팽이가 있다. 이 달팽이는 높이가 V미터인 나무 막대를 올라갈 것이다.
+
+달팽이는 낮에 A미터 올라갈 수 있다. 하지만, 밤에 잠을 자는 동안 B미터 미끄러진다. 
+
+또, 정상에 올라간 후에는 미끄러지지 않는다.
+
+달팽이가 나무 막대를 모두 올라가려면, 며칠이 걸리는지 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 세 정수 A, B, V가 공백으로 구분되어서 주어진다. (1 ≤ B < A ≤ V ≤ 1,000,000,000)
+
+출력
+첫째 줄에 달팽이가 나무 막대를 모두 올라가는데 며칠이 걸리는지 출력한다.*/
+
+public class _2869 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int V = Integer.parseInt(st.nextToken());
+		int cnt;
+		cnt = (V - A) / (A - B) + 1;
+		if ((V - A) % (A - B) != 0)
+			cnt++;
+		bw.write(cnt + "");
+		bw.flush();
+		bw.close();
+		br.close();
+
+	}
+}
